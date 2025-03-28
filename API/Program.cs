ba91ecf5cfb778d6854fce33c816689aba1d5bc1
Roy <roywmraziv@icloud.com>
2025-03-28 10:17:59 -0500

    added microsoft@signalr and install tslib

--- API/Program.cs
+++ API/Program.cs
@@ -2,6 +2,7 @@ using API.Data;
 using API.Extensions;
 using API.Middleware;
 using API.Models;
+using API.SignalR;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -21,23 +22,26 @@ var app = builder.Build();
 
 app.UseMiddleware<ExceptionMiddleware>();
 
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHub<PresenceHub>("hubs/presence");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
-try{
+try
+{
     var context = services.GetRequiredService<DataContext>();
     var userManager = services.GetRequiredService<UserManager<AppUser>>();
     var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
     await context.Database.MigrateAsync();
     await Seed.SeedUsers(userManager, roleManager);
 }
-catch(Exception ex)
+catch (Exception ex)
 {
     var logger = services.GetRequiredService<ILogger<Program>>();
     logger.LogError(ex, "An error occured during migration");
---
+++ API/SignalR/PresenceHub.cs
@@ -0,0 +1,21 @@
+using System;
+using API.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace API.SignalR;
+
+[Authorize]
+public class PresenceHub : Hub
+{
+    public async override Task OnConnectedAsync()
+    {
+        await Clients.Others.SendAsync("UserIsOnline", Context.User?.GetUsername());
+    }
+
+    public async override Task OnDisconnectedAsync(Exception? exception)
+    {
+        await Clients.Others.SendAsync("UserIsOffline", Context.User?.GetUsername());
+        await base.OnDisconnectedAsync(exception);
+    }
+}
